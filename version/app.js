78de1175af707dae7cf2c709aff4cfecde18ce82
etarkamo <eetu.tarkamo@helsinki.fi>
2020-05-19 11:26:02 +0300

    updated to version 3

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 2 of the app. Yeah son!' + '\n';
+    response = 'This is the mothafukken version 3 of the app!' + '\n';
 
     //send the response to the client
     res.send(response);
